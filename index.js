58b0ceadd5daa4a84f330147eddc1bef006e953a
Blake Griffith <blake.a.griffith@gmail.com>
2023-08-30 11:10:57 +0200

    Test basic hypercore and hyperbee

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-let Parser = require('rss-parser');
+import Hypercore from 'hypercore';
+import Parser from 'rss-parser';
+import { Items } from './src/items.js';
 let parser = new Parser();
+
+const urls = ['https://www.reddit.com/.rss', 'https://xkcd.com/rss.xml', 'https://feeds.soundcloud.com/users/soundcloud%3Ausers%3A211911700/sounds.rss'];
 
 (async () => {
 
-  let feed = await parser.parseURL('https://www.reddit.com/.rss');
+  const core = new Hypercore('./writer-storage')
+  await core.ready()
+  console.log('hypercore key:', core.key);
+  for (const url of urls) {
+  let feed = await parser.parseURL(url);
   console.log(feed.title);
-
   feed.items.forEach(item => {
+    console.log(item);
     console.log(item.title + ':' + item.link)
   });
+  }
+
+  //console.log(feed)
+
+
+  //feed.items.forEach(item => {
+  //  console.log(item);
+  //  console.log(item.title + ':' + item.link)
+  //});
 
 })();
