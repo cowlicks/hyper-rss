10d67194c5c7ee72fdeb8b820177e5c6479b379c
Blake Griffith <blake.a.griffith@gmail.com>
2023-08-31 11:30:02 +0200

    Add basic adding of items to hrss feed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import Hypercore from 'hypercore';
 import Parser from 'rss-parser';
-import { Items } from './items.js';
+import { Items, itemsNotHyperized } from './items.js';
 let parser = new Parser();
 
-const urls = ['https://www.reddit.com/.rss', 'https://xkcd.com/rss.xml', 'https://feeds.soundcloud.com/users/soundcloud%3Ausers%3A211911700/sounds.rss'];
+const urls = [
+//'https://www.reddit.com/.rss',
+'https://xkcd.com/rss.xml',
+//'https://feeds.soundcloud.com/users/soundcloud%3Ausers%3A211911700/sounds.rss'
+];
 
 (async () => {
 
-  const core = new Hypercore('./writer-storage')
-  await core.ready()
-  console.log('hypercore key:', core.key);
+  const core = new Hypercore('./writer-storage');
+  await core.ready();
+  const items = new Items({core});
+
   for (const url of urls) {
-  let feed = await parser.parseURL(url);
-  console.log(feed.title);
-  feed.items.forEach(item => {
-    console.log(item);
-    console.log(item.title + ':' + item.link)
-  });
+    let feed = await parser.parseURL(url);
+    const missing = await itemsNotHyperized(feed.items, items.db);
+    const batcher = items.db.batch();
+    for (const {key, rssItem} of missing) {
+      console.log(key, rssItem)
+      await batcher.put(key, JSON.stringify(rssItem));
+    }
+    await batcher.flush();
   }
-
-  //console.log(feed)
-
-
-  //feed.items.forEach(item => {
-  //  console.log(item);
-  //  console.log(item.title + ':' + item.link)
-  //});
 
 })();
--- src/items.js
+++ src/items.js
@@ -7,4 +7,35 @@ export class Items {
   }
 }
 
+const defaultRssItemHasher = ({ guid }) => guid
+
+// TODO use Hyperbee.batch
+export async function itemsNotHyperized(rssFeed, hyperbeeItemsDb, { hasher = defaultRssItemHasher } = {}) {
+  const out = []
+  let dbReady = false;
+  for (const rssItem of [...rssFeed].reverse()) {
+    if (!dbReady) {
+      await hyperbeeItemsDb.ready();
+      dbReady = true;
+    }
+
+    const key = hasher(rssItem)
+    const hyperItemRes = await hyperbeeItemsDb.get(key)
+    if (hyperItemRes === null) {
+      console.log(`No key: ${key}`);
+      out.push({key, rssItem})
+    } else {
+    /*
+      console.log(`
+        HAS key ${key}
+        hitem ${hyperItemRes}
+        ritem ${rssItem.title}
+      `);
+    */
+    }
+  }
+  return out
+}
+
+
 console.log(Hyperbee);
