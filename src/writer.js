f799edebc875fd2b4c88d570ae96c5375acf7f6c
Blake Griffith <blake.a.griffith@gmail.com>
2023-09-08 15:50:49 +0200

    Create writer from config or new from url

--- src/writer.js
+++ src/writer.js
@@ -1,16 +1,13 @@
-import Hyperswarm from 'hyperswarm';
+import { stat } from 'node:fs/promises';
 import Parser from 'rss-parser';
 import Corestore from 'corestore';
 import Hyperbee from 'hyperbee';
 
-import goodbye from 'graceful-goodbye';
-
-import { base64FromBuffer } from './utils/index.js';
+import { base64FromBuffer, readJsonFile, writeJsonFile } from './utils/index.js';
 import { log } from './log.js';
 import { itemsNotHyperized } from './items.js';
 import { getEnclosure } from './blobs.js';
 import { swarmInit } from './swarm.js';
-import { withTmpDir } from './test.js';
 
 const WRITER_STORAGE = './writer-storage';
 const HRSS_STORE_PREFIX = 'hrss';
@@ -18,6 +15,8 @@ const HRSS_KEYS_STORE_SUFFIX = 'keys';
 const HRSS_FEED_STORE_SUFFIX = 'feed';
 const HRSS_BLOB_KEYS_STORE_SUFFIX = 'blobKeys';
 const HRSS_BLOB_STORE_SUFFIX = 'blob';
+
+const DEFAULT_WRITER_CONFIG_FILE_NAME = './writer-config.json';
 
 export function storeNames ({
   prefix = HRSS_STORE_PREFIX,
@@ -35,8 +34,9 @@ export function storeNames ({
 }
 
 export function getStore ({ storageName = WRITER_STORAGE } = {}) {
+  log.info(`Initializing Corestore at = [${storageName}]`);
   const store = new Corestore(storageName);
-  return { store };
+  return { store, storageName };
 }
 
 function getCores (store, { ...rest } = {}) {
@@ -49,13 +49,13 @@ function getCores (store, { ...rest } = {}) {
 }
 
 export function getStoreAndCores ({ ...opts } = {}) {
-  const { store } = getStore({ ...opts });
+  const { store, ...storeRest } = getStore({ ...opts });
   const cores = getCores(store, { ...opts });
-  return { store, cores };
+  return { store, cores, ...storeRest };
 }
 
-async function initWriter ({ ...opts } = {}) {
-  const { store, cores: { keys, feed, blobKeys, blobs } } = getStoreAndCores({ ...opts });
+async function initWriter (url, { ...opts } = {}) {
+  const { store, cores: { keys, feed, blobKeys, blobs }, ...storeAndCoreRest } = getStoreAndCores({ ...opts });
 
   await Promise.all([keys.ready(), feed.ready(), blobKeys.ready(), blobs.ready()]);
 
@@ -82,7 +82,8 @@ async function initWriter ({ ...opts } = {}) {
       feed: new Hyperbee(feed),
       blobKeys: new Hyperbee(blobKeys),
       blobs: new Hyperbee(blobs)
-    }
+    },
+    ...storeAndCoreRest
   };
 }
 
@@ -104,14 +105,36 @@ async function addMissing (missing, { feed, blobKeys: _, blobs }) {
   await Promise.all([feedBatcher.flush(), blobsBatcher.flush()]);
 }
 
+const fromConfigPropertyName = 'fromConfig';
+function setFromConfig (o) {
+  o[fromConfigPropertyName] = true;
+}
+
+function isFromConfig (o) {
+  return !!o[fromConfigPropertyName];
+}
+
 // TODO rewrite this to use Keyed blobs
+// TODO Writer should load URL from store if it exists. Otherwise we should
+// only provide a URL for a brand new Writer
 export class Writer {
-  constructor (url, opts = {}) {
+  static async fromConfig (configFileName = DEFAULT_WRITER_CONFIG_FILE_NAME) {
+    const { url, ...opts } = await readJsonFile(configFileName);
+    const writer = new Writer(url, { ...opts });
+    setFromConfig(writer);
+    return writer;
+  }
+
+  static async forNewUrl (url, opts) {
+    return new Writer(url, opts);
+  }
+
+  constructor (url, { configFileName = DEFAULT_WRITER_CONFIG_FILE_NAME, ...opts } = {}) {
     log.info(`Creating writer for URL = [${url}]`);
     const parser = new Parser();
     Object.assign(
       this,
-      { url, opts, parser }
+      { url, configFileName, opts, parser }
     );
   }
 
@@ -120,11 +143,27 @@ export class Writer {
     Object.assign(
       this,
       {
-        ...(await initWriter(this.opts)),
+        ...(await initWriter(this.url, this.opts)),
         parsedRssFeed
       }
     );
+    await this.maybeSaveConfig();
     return this;
+  }
+
+  async close () {
+    await Promise.all([
+      this.cores.keys.close(),
+      this.cores.feed.close(),
+      this.cores.blobKeys.close(),
+      this.cores.blobs.close(),
+      this.cores.keys.close(),
+      this.bTrees.feed.close(),
+      this.bTrees.blobKeys.close(),
+      this.bTrees.blobs.close(),
+      this.swarm.destroy(),
+      this.store.close()
+    ]);
   }
 
   getMissing () {
@@ -144,14 +183,29 @@ export class Writer {
   discoveryKeyString () {
     return base64FromBuffer(this.cores.keys.key);
   }
+
+  async maybeSaveConfig (...args) {
+    if (isFromConfig(this)) {
+      return;
+    }
+    await this.saveConfig(...args);
+  }
+
+  async saveConfig (configFileName = (this.configFileName ?? DEFAULT_WRITER_CONFIG_FILE_NAME)) {
+    console.log('saving config to ', configFileName);
+    return await writeJsonFile(configFileName, {
+      url: this.url,
+      storageName: this.storageName,
+      discoveryKeyString: this.discoveryKeyString
+    });
+  }
 }
 
 export async function _testUpdateWriterIntegration (tmpd) {
   const url = 'https://xkcd.com/rss.xml';
-  const writer = new Writer(url, { storageName: tmpd });
+  const writer = await Writer.fromConfig();
   await writer.init();
   await writer.updateFeed();
+  console.log('WRITER LENGTH', writer.cores.feed.length);
   return writer.discoveryKeyString();
 }
-
-// (async () => await withTmpDir(async (tmpd) => await _testUpdateWriterIntegration(tmpd)))();
