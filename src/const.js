24f36cd764792d3a5e11d37d25cd56bf99c47547
Blake Griffith <blake.a.griffith@gmail.com>
2023-09-08 20:37:52 +0200

    Add const.js

---
+++ src/const.js
@@ -0,0 +1,6 @@
+export const TEST_URLS = {
+  reddit: 'https://www.reddit.com/.rss',
+  xkcd: 'https://xkcd.com/rss.xml',
+  chapo: 'https://feeds.soundcloud.com/users/soundcloud%3Ausers%3A211911700/sounds.rss',
+  skeptoid: 'https://feed.skeptoid.com/'
+};
