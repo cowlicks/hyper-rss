fdd28ef5684f801bb319c011171f6050996b45cf
Blake Griffith <blake.a.griffith@gmail.com>
2023-10-03 12:10:44 +0200

    Rm comma dangle lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
   ],
   rules: {
     'one-var': 0,
-    'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }]
+    'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    'comma-dangle': ['error', 'always-multiline' ],
   }
 
 };
