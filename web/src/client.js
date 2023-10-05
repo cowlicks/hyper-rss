50f1e97d8c319dadab5211f40a8a305c2dca44c5
Blake Griffith <blake.a.griffith@gmail.com>
2023-10-05 13:53:15 +0200

    Make get client as a singleton

--- web/src/client.js
+++ web/src/client.js
@@ -4,11 +4,16 @@ import { createContext, useContext, useEffect, useState } from 'react';
 export const ApiContext = createContext();
 const URL = 'ws://localhost:8080';
 
+let _client = null;
+function getClient () {
+  if (_client === null) {
+    _client = WebClient.fromUrl(URL);
+  }
+  return _client;
+}
+
 export function ApiProvider ({ children }) {
-  const [client] = useState(() => WebClient.fromUrl(URL));
-  useEffect(() => {
-    return () => client.shutDown();
-  }, [client]);
+  const [client] = useState(() => getClient());
   return (
     <ApiContext.Provider value={client}>
       {children}
