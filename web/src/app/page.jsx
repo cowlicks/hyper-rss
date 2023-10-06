52399538a5133b65bbbbb45b7104bf89236f5ac4
Blake Griffith <blake.a.griffith@gmail.com>
2023-10-06 12:57:09 +0200

    Add feed components and link to a feed

--- web/src/app/page.jsx
+++ web/src/app/page.jsx
@@ -15,20 +15,47 @@ function useRpcCall (method, params) {
   return data;
 }
 
-function useGetFeedMetadata () {
+function useGetFeedsMetadata () {
   const [params] = useState([{ wait: false, update: false }]);
   return useRpcCall('getFeedsMetadata', params);
 }
 
-function Feed () {
-  const metadata = useGetFeedMetadata();
-  return (<code><pre>{ metadata.loading ? '...loading' : JSON.stringify(metadata.data, null, 2)}</pre></code>);
+function Feeds () {
+  const feedsMetadata = useGetFeedsMetadata();
+  return (<code><pre>{ feedsMetadata.loading ? '...loading' : feedsMetadata.data.result.map(x => (<Feed key={x[0]} metadata={x} />))}</pre></code>);
 }
+
+function Feed ({ metadata }) {
+  const [discoveryKey, feedAttributes] = metadata;
+  return (
+      <div className="Feed">
+        <div className="Feed__DiscoveryKey">
+          <DiscoveryKey {...{ discoveryKey }} />
+        </div>
+        <div className="Feed__FeedAttributes">
+          <FeedAttributes {...{ feedAttributes }} />
+        </div>
+      </div>
+  );
+}
+
+function DiscoveryKey ({ discoveryKey }) {
+  return (<a href={`feed/${discoveryKey}`} className="DiscoveryKey">{discoveryKey}</a>);
+}
+
+function FeedAttributes ({ feedAttributes }) {
+  return (
+    <div className="FeedAttributes">
+      {JSON.stringify(feedAttributes, null, 2)}
+    </div>
+  );
+}
+
 export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ApiProvider>
-      <Feed/>
+      <Feeds/>
       </ApiProvider>
     </main>
   );
