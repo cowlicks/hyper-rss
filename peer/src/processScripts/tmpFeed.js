d6105feaa4da0872c9d21dfc3668558ab1c035fc
Blake Griffith <blake.a.griffith@gmail.com>
2023-10-09 15:59:40 +0200

    Name tmp rss process for easy finding.

--- peer/src/processScripts/tmpFeed.js
+++ peer/src/processScripts/tmpFeed.js
@@ -5,6 +5,7 @@ import { withTmpRssFeed } from '../tools/mirror.js';
 (async () => {
   const waitUntil = Deferred();
   const feedName = process.argv[2] ?? XKCD;
+  process.title = `tmpfeed:${feedName}`;
   process.on('message', (msg) => {
     if (msg === STOP_PROCESS) {
       waitUntil.resolve();
