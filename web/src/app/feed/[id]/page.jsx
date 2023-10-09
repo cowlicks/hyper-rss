8dc139df57b19bc604dffe7b6e792aa3c188504a
Blake Griffith <blake.a.griffith@gmail.com>
2023-10-09 16:06:45 +0200

    Extract, sanitize, and display rss items

--- web/src/app/feed/[id]/page.jsx
+++ web/src/app/feed/[id]/page.jsx
@@ -1,11 +1,31 @@
 'use client';
+import * as DOMPurify from 'dompurify';
 import { useGetReaderFeed } from '@/client';
 
 export default function Feed ({ params: { id } }) {
-  const result = useGetReaderFeed(id);
+  const { loading, data, error } = useGetReaderFeed(id);
+  if (loading) {
+    return <div>...loading</div>;
+  }
+  if (error) {
+    return <div>Error: {JSON.stringify(error, null, 2)}</div>;
+  }
+
   return (
     <div>
-    {JSON.stringify(result, null, 2)}
+    {data.map(({ seq, key, value }, index) => {
+      const k = Buffer.from(key.data);
+      const obj = JSON.parse(Buffer.from(value.data).toString());
+      const safeContent = {
+        __html: DOMPurify.sanitize(obj.content),
+      };
+      return (
+        <div key={index}>
+          <div>{obj.title}</div>
+          <div dangerouslySetInnerHTML={safeContent} />
+        </div>
+      );
+    })}
     </div>
   );
 }
