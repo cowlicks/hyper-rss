1ec5f28e89f13a4b429870304d2f3198171b66ce
Blake Griffith <blake.a.griffith@gmail.com>
2023-10-11 11:50:59 +0200

    get blobs in update-test-data

--- aggregator/src/bin/update-test-data.js
+++ aggregator/src/bin/update-test-data.js
@@ -5,19 +5,23 @@ import { withReader, withUpdatedWriter } from '@hrss/peer/src/utils/tests.js';
 import { AGG_DATA_DIR_WITH_TWO_FEEDS } from '../const.js';
 import { join } from 'node:path';
 
+// TODO can we replace all this this getFoo with an update?
 await withUpdatedWriter(XKCD, async (xkcdWriter) => {
   await withReader(xkcdWriter.discoveryKeyString(), async (xkcdReader) => {
     console.log(await xkcdReader.getMetadata());
     console.log(await xkcdReader.getFeed());
+    console.log(await xkcdReader.getKeysAndBlobs());
     await withUpdatedWriter(CHAPO, async (chapoWriter) => {
       await withReader(chapoWriter.discoveryKeyString(), async (chapoReader) => {
         console.log(await chapoReader.getMetadata());
         console.log(await chapoReader.getFeed());
+        console.log(await chapoReader.getKeysAndBlobs());
         await withUpdatedWriter(SKEPTOID, async (skeptoidWriter) => {
           await withReader(skeptoidWriter.discoveryKeyString(), async (skeptoidReader) => {
             console.log(await skeptoidReader.getMetadata());
             console.log(await skeptoidReader.getFeed());
 
+            console.log(await skeptoidReader.getKeysAndBlobs());
             console.log('Updating test data');
             await Promise.all([
               cp(xkcdReader.storageName, join(AGG_DATA_DIR_WITH_TWO_FEEDS, xkcdWriter.discoveryKeyString()), { recursive: true }),
